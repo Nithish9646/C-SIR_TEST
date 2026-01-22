95d9474a651aa1c1e988e68341ba9e9e3195a287
NITHISH P <nithishp9646@gmail.com>
2026-01-22 12:59:20 +0530

    Add files via upload

---
+++ multiple.cpp
@@ -0,0 +1,72 @@
+//NITHISH P
+//V25CE1N2
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		int x=10;
+	public:
+		A()
+		{
+			cout<<"default constructor of class A"<<endl;
+		}
+		~A()
+		{
+			cout<<"destructor of class A"<<endl;
+		}
+};
+class B
+{
+	public:
+		int y=20;
+	public:
+		B()
+		{
+			cout<<"default constructor of class B"<<endl;
+		}
+		~B()
+		{
+			cout<<"destructor of class B"<<endl;
+		}
+};
+class C
+{
+	public:
+		int z=30;
+	public:
+		C()
+		{
+			cout<<"default constructor of class C"<<endl;
+		}
+		~C()
+		{
+			cout<<"destructor of class C"<<endl;
+		}
+};
+class D:public A,public B,public C
+{
+	public:
+		int m=40;
+	public:
+		D()
+		{
+			cout<<"default constructor of class D"<<endl;
+		}
+		~D()
+		{
+			cout<<"destructor of class D"<<endl;
+		}
+		void print()
+		{
+			cout<<"x="<<x<<endl;
+			cout<<"y="<<y<<endl;
+			cout<<"z="<<z<<endl;
+			cout<<"m="<<m<<endl;
+		}
+};
+int main()
+{
+	D ob;
+	ob.print();
+}
---
+++ stack.cpp
@@ -0,0 +1,89 @@
+//NITHISH P
+//V25CE1N2
+#include<iostream>
+using namespace std;
+template<class ty>
+class A
+{
+	public:
+	ty a[3];
+	ty max=3;
+        ty top=0;
+	public:
+	void push()
+	{
+		try
+		{
+			if(top==max)
+			{
+				throw "stack is full";
+			}
+			else
+			{
+				ty ele;
+				cout<<"enter the value for ele : ";
+					cin>>ele;
+				a[top++]=ele;
+			}
+		}
+		catch (const char *s)
+		{
+			cout<<"s="<<s<<endl;
+		}
+		
+	}
+	void pop()
+	{
+		try
+		{
+			if(top==0)
+			{
+				throw "stack is underflow";
+			}
+			else
+			{
+				a[--top]=0;
+			}
+		}
+		catch (const char *s)
+		{
+			cout<<"s="<<s<<endl;
+		}
+		
+	}
+	void display()
+	{
+		ty i;
+		for(i=0;i<max;i++)
+		{
+			cout<<"a["<<i<<"]="<<a[i]<<endl;
+		}
+	}
+};
+int main()
+{
+	A <int> ob;
+	int op;
+	while(1)
+	{
+		cout<<"1) push , 2) pop , 3) display  , 4) exit "<<endl;
+		cout<<"enter the option : ";
+		cin>>op;
+
+		switch(op)
+		{
+			case 1:
+				ob.push();
+				break;
+			case 2:
+				ob.pop();
+				break;
+			case 3: ob.display();
+				break;
+			case 4: return 0;
+				break;
+			default:cout<<"invalid option"<<endl;
+				break;
+		}
+	}
+}
